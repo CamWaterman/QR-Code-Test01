f00b7c125c5473bc5ea089a973fc6986c6b6588e
CamWaterman <cameron.waterman@ttu.edu>
2014-10-08 18:05:30 -0500

    layout and some skeleton

--- app/src/main/java/sdc/qrcodetest01/HomeScreen.java
+++ app/src/main/java/sdc/qrcodetest01/HomeScreen.java
@@ -4,17 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 public class HomeScreen extends Activity {
+
+    Button captureBtn;
+    TextView scoreView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        captureBtn = (Button) findViewById(R.id.captureBtn);
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        scoreView = (TextView) findViewById(R.id.scoreBody);
+        scoreView.setText(QR.score);
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
